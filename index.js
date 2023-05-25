310928c63f0f7f2fcbff421f792d7410ab20c66a
Nikki Little <nikkilittle13@gmail.com>
2023-05-25 13:48:51 -0400

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+  }
+
+  return messages;
+}
+
+function countDown(number) {
+  for (let countDown = number; countDown >= 0; countDown--) {
+    console.log(countDown);
+}
+}
+
+
