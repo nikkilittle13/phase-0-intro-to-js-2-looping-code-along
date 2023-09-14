50afb1de2a0c10f59b322411bd6a23aa928ce7ed
Nikki Little <nikkilittle13@gmail.com>
2023-09-14 14:06:23 -0400

    creates myfile.md

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ function writeCards(names) {
   for (let i = 0; i < names.length; i++) {
     messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
   }
-
   return messages;
 }
 
 function countDown(number) {
   for (let countDown = number; countDown >= 0; countDown--) {
     console.log(countDown);
+  }
 }
-}
-
-
